62fcfe0650bf6842edf90716d62087e8e88799dd
Emi <emi.serban@gmail.com>
2025-11-29 14:37:09 +0200

    Updated OuttakeTuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidtests/OuttakeTuning.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidtests/OuttakeTuning.kt
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.pidtests
+package org.firstinspires.ftc.teamcode.teleop.tests
 
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.config.Config
@@ -13,25 +13,25 @@ import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.teamcode.library.TimeKeep
 import org.firstinspires.ftc.teamcode.library.controller.PIDController
+import org.firstinspires.ftc.teamcode.robot.Robot
+import org.firstinspires.ftc.teamcode.robot.Shooter
+import org.firstinspires.ftc.teamcode.teleop.prepPositions.OuttakeTest.outtakeConfig
 import kotlin.math.abs
+import kotlin.time.toDuration
 
 @TeleOp
 class OuttakeTuning : LinearOpMode() {
     @Config
-    data object outtakeConfig {
-        @JvmField var sampleWindow = 0.1
-        @JvmField var TICKS_PER_REV = 8192.0
-        @JvmField var targetRPM = 3400
-    }
-    @Config
     data object OuttakeTuningConfig {
         @JvmField
         var controller = PIDController(
-            kP = 0.001995,
-            kD = 0.0000001,
-            kI = 0.0000001,
-            stabilityThreshold = 0.2
+            kP = 0.0004,
+            kD = 0.00002,
+            kI = 0.000005,
+            stabilityThreshold = 50.0
         )
+        @JvmField
+        var kV = 0.00025
         @JvmField
         var targetRpm = 0.0
     }
@@ -56,6 +56,8 @@ class OuttakeTuning : LinearOpMode() {
         var lastTime = now()
         var lastResetTime = now()
         val timeKeep = TimeKeep()
+        var ff = 0.0
+        var pid = 0.0
         var rpm = 0.0
         var targetRpm = 0.0
         var shooterPower = 0.0
@@ -81,8 +83,10 @@ class OuttakeTuning : LinearOpMode() {
 
             targetRpm = OuttakeTuningConfig.targetRpm
 
-            shooterPower = OuttakeTuningConfig.controller.calculate(abs(rpm),abs(targetRpm),timeKeep.deltaTime)
-            /// how to target toleramce???
+            ff = OuttakeTuningConfig.kV * targetRpm
+            pid = OuttakeTuningConfig.controller .calculate(rpm, targetRpm, timeKeep.deltaTime)
+            shooterPower = (ff + pid).coerceIn(-1.0, 1.0)
+
             motorShooterBottom.power = shooterPower
             motorShooterTop.power = shooterPower
 
