3fb73b75fda494138110a32b2ce37b1da5de5697
david <davidalexandrucomsa@gmail.com>
2025-12-29 16:41:24 +0200

    Added auto Path in meep meep

--- MeepMeep/src/main/java/com/meep/MeepMeep.kt
+++ MeepMeep/src/main/java/com/meep/MeepMeep.kt
@@ -15,20 +15,51 @@ fun main() {
     System.setProperty("sun.java2d.opengl", "true")
 
     val startPose = Pose(63.inch, -11.inch, 180.0.deg)
-    Pose smallTrianglePose = Pose(57.inch, -10.inch, 210.0.deg)
+    val smallTrianglePose = Pose(57.inch, -10.inch, 210.0.deg)
+    val bigTrianglePose = Pose(-14.inch, -14.inch, 225.0.deg)
+    val rightIntakePose = Pose(36.inch, -35.inch, 270.0.deg)
+    val middleIntakePose = Pose(12.inch, -35.inch, 270.0.deg)
+    val leftIntakePose = Pose(-12.inch, -35.inch, 270.0.deg)
+    val endPose = Pose(16.inch, -54.inch, 0.0.deg)
 
     val meepMeep = MeepMeep(600)
 
     val myBot =
         DefaultBotBuilder(meepMeep)
             .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(180.0), 15.0)
-            .setDimensions(15.748,15.748)
+            .setDimensions(16.5,16.5)
             .setStartPose(startPose.pose2d)
             .build()
 
 
     myBot.runAction(myBot.drive.actionBuilder(startPose.pose2d).ex()
-        .lineToSpl
+        .splineToLinearHeading(smallTrianglePose, 50.deg)
+        .waitSeconds(1.0)
+
+        .strafeToLinearHeading(rightIntakePose)
+        .waitSeconds(1.0)
+        .lineToY(-40.inch)
+        .waitSeconds(1.0)
+        .lineToY(-45.inch)
+        .strafeToLinearHeading(smallTrianglePose)
+        .waitSeconds(1.0)
+
+        .strafeToLinearHeading(middleIntakePose)
+        .setTangent(-90.deg)
+        .lineToY(-40.inch)
+        .waitSeconds(1.0)
+        .lineToY(-45.inch)
+        .strafeToLinearHeading(bigTrianglePose)
+
+        .strafeToLinearHeading(leftIntakePose)
+        .setTangent(-90.deg)
+        .lineToY(-40.inch)
+        .waitSeconds(1.0)
+        .lineToY(-45.inch)
+        .strafeToLinearHeading(bigTrianglePose)
+
+        .strafeToLinearHeading(endPose)
+
         .build()
     )
 
