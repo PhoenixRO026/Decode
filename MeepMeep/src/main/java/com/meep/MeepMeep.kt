1b11eb6a3b5fd76e821bd8382ac9fa71345bab6b
david <davidalexandrucomsa@gmail.com>
2026-01-02 13:07:40 +0200

    meepmeep

--- MeepMeep/src/main/java/com/meep/MeepMeep.kt
+++ MeepMeep/src/main/java/com/meep/MeepMeep.kt
@@ -1,4 +1,3 @@
-//RedSmallTriangleStart meepmeep
 @file:JvmName("MeepMeep")
 package com.meep
 
@@ -19,20 +18,16 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
 fun main() {
     System.setProperty("sun.java2d.opengl", "true")
 
-    val startPose = Pose(-63.inch, -11.inch, 0.0.deg)
-    val smallTrianglePose = Pose(57.inch, -10.inch, (200.0).deg)
-    val bigTrianglePose = Pose(-10.inch, -10.inch, (225.0).deg)
-
-    val rightIntakePose = Pose(36.inch, -30.inch, (270.0).deg)
-    val middleIntakePose = Pose(12.inch, -30.inch, (270.0).deg)
-    val leftIntakePose = Pose(-12.inch, -30.inch, (270.0).deg)
-
-    val endPose = Pose(10.inch, -58.inch, -140.0.deg)
-
+    val startPose = Pose(63.inch, -11.inch, 180.0.deg)
+    val smallTrianglePose = Pose(57.inch, -10.inch, 200.0.deg)
+    val bigTrianglePose = Pose(-6.inch, -6.inch, 225.0.deg)
+    val rightIntakePose = Pose(36.inch, -30.inch, 270.0.deg)
+    val middleIntakePose = Pose(12.inch, -30.inch, 270.0.deg)
+    val leftIntakePose = Pose(-12.inch, -30.inch, 270.0.deg)
+    val endPose = Pose(16.inch, -54.inch, 0.0.deg)
     val shootingTime = 4.2
 
-
-    val meepMeep = MeepMeep(950)
+    val meepMeep = MeepMeep(600)
 
     val kinematics = MecanumKinematics(
         15.0,
@@ -54,35 +49,32 @@ fun main() {
             .build()
 
 
-    myBot.runAction(
-        myBot.drive.actionBuilder(startPose.pose2d).ex()
+    myBot.runAction(myBot.drive.actionBuilder(startPose.pose2d).ex()
+        .splineToLinearHeading(smallTrianglePose, 50.deg)
+        .waitSeconds(shootingTime)
 
-            .strafeToLinearHeading(bigTrianglePose)
-            .waitSeconds(shootingTime)
+        .strafeToLinearHeading(rightIntakePose)
+        .setTangent(-90.deg)
+        .lineToY(-45.inch, slowSpeed)
+        .strafeToLinearHeading(smallTrianglePose)
+        .waitSeconds(shootingTime)
 
-            .strafeToLinearHeading(leftIntakePose)
-            .setTangent(-90.deg)
-            .lineToY(-45.inch, slowSpeed)
-            .strafeToLinearHeading(bigTrianglePose)
-            .waitSeconds(shootingTime)
+        .strafeToLinearHeading(middleIntakePose)
+        .setTangent(-90.deg)
+        .lineToY(-45.inch, slowSpeed)
+        .strafeToLinearHeading(bigTrianglePose)
+        .waitSeconds(shootingTime)
 
-            .strafeToLinearHeading(middleIntakePose)
-            .setTangent(-90.deg)
-            .lineToY(-45.inch, slowSpeed)
-            .strafeToLinearHeading(bigTrianglePose)
-            .waitSeconds(shootingTime)
+        .strafeToLinearHeading(leftIntakePose)
+        .setTangent(-90.deg)
+        .lineToY(-45.inch, slowSpeed)
+        .strafeToLinearHeading(bigTrianglePose)
+        .waitSeconds(shootingTime)
 
-            .strafeToLinearHeading(rightIntakePose)
-            .setTangent(-90.deg)
-            .lineToY(-45.inch, slowSpeed)
-            .strafeToLinearHeading(smallTrianglePose)
-            .waitSeconds(shootingTime)
+        .strafeToLinearHeading(endPose)
 
-            .strafeToLinearHeading(endPose)
-
-            .build()
+        .build()
     )
-
 
     meepMeep.setBackground(Background.FIELD_DECODE_JUICE_DARK)
         .setDarkMode(true)
