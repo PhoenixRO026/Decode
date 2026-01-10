f3ab0b0569c5d54c52d2023f548d11bef4d8213f
ProNoob1029 <pronoob1029@gmail.com>
2026-01-11 00:53:14 +0200

    replay might work?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestPsiKit.kt
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.psilynx.psikit.core.Logger
+import org.psilynx.psikit.ftc.PsiKitOpMode
+
+@TeleOp
+class TestPsiKit: PsiKitOpMode() {
+    override fun psiKit_init() {
+    }
+
+    override fun psiKit_init_loop() {
+
+    }
+
+    override fun psiKit_start() {
+
+    }
+
+    override fun psiKit_loop() {
+
+    }
+
+    override fun psiKit_stop() {
+
+    }
+}
---
+++ TeamCode/src/test/java/test/ReplayTest.kt
@@ -0,0 +1,19 @@
+package test
+
+import org.firstinspires.ftc.teamcode.teleop.TestPsiKit
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.psilynx.psikit.core.rlog.RLOGReplay
+import org.psilynx.psikit.ftc.Replay
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@Config(shadows = [ShadowAppUtil::class])
+@RunWith(RobolectricTestRunner::class)
+class ReplayTest {
+    @Test
+    fun run() {
+        val replaySource = RLOGReplay("testLog.rlog")
+        Replay(TestPsiKit(), replaySource).run()
+    }
+}
