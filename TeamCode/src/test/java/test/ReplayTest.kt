9640a39f066a2b6a4d122de4b7bf3e439695d2b8
Dragos Morar <pronoob1029@gmail.com>
2026-01-12 09:51:23 +0200

    filepath

--- TeamCode/src/test/java/test/ReplayTest.kt
+++ TeamCode/src/test/java/test/ReplayTest.kt
@@ -13,7 +13,7 @@ import org.robolectric.annotation.Config
 class ReplayTest {
     @Test
     fun run() {
-        val replaySource = RLOGReplay("testLog.rlog")
+        val replaySource = RLOGReplay(RLOGReplay.promptForPath())
         Replay(TestPsiKit(), replaySource).run()
     }
 }
