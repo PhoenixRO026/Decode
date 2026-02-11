8256ab764983f86ce6eee890e467f662eeafae2b
Coman <comanrazvanioan@gmail.com>
2026-02-11 14:44:43 +0200

    servo pos tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidtests/ServoTunning.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidtests/ServoTunning.kt
@@ -9,16 +9,16 @@ import org.firstinspires.ftc.teamcode.library.TimeKeep
 @TeleOp
 class ServoTunning: LinearOpMode() {
     override fun runOpMode() {
-        val servoFront = hardwareMap.get(Servo::class.java, "servoFront")
-        val servoBack = hardwareMap.get(Servo::class.java, "servoBack")
+        val servoFront = hardwareMap.get(Servo::class.java, "servoTransferFront")
+        val servoBack = hardwareMap.get(Servo::class.java, "servoTransferBack")
 
         val timeKeep = TimeKeep()
         var previousTime: Double
         var deltaTime : Double
         var now : Double
 
-        servoFront.position = 0.5
-        servoBack.position = 0.5
+        servoFront.position = 0.0
+        servoBack.position = 0.0
 
         waitForStart()
         previousTime = now()
@@ -29,17 +29,14 @@ class ServoTunning: LinearOpMode() {
             previousTime = now
             if(gamepad1.dpad_up) {
                 servoFront.position += deltaTime * 0.1
+                servoBack.position += deltaTime * 0.1
             }
             if(gamepad1.dpad_down) {
                 servoFront.position -= deltaTime * 0.1
-            }
-            if(gamepad1.dpad_right) {
-                servoBack.position += deltaTime * 0.1
-            }
-            if(gamepad1.dpad_left) {
                 servoBack.position -= deltaTime * 0.1
             }
-
+            telemetry.addData("pos ", servoFront.position)
+            telemetry.update()
         }
 
     }
