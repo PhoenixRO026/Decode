a07bbb8e5536d0a3c257e9b3cd44d443c077f1a6
david <davidalexandrucomsa@gmail.com>
2026-02-20 15:02:37 +0200

    shooter work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.kt
@@ -43,15 +43,15 @@ class Shooter(
             kI = 0.00045,
             stabilityThreshold = 0.2
         )
-        @JvmField var ticksPerRev = 8192 * (15/22)
+        @JvmField var ticksPerRev = 8192.0 * (15.0/22.0)
 
-        @JvmField var targetPosTolerance = 3
+        @JvmField var targetPosTolerance = 3.0
         @JvmField var minTurretPosition = -14000.0
         @JvmField var maxTurretPosition = 13300.0
-        @JvmField var limitTolerence = 5
+        @JvmField var limitTolerence = 5.0
     }
 
-    val rpm get() = encoderOuttake.getPositionAndVelocity().velocity / 28.0 * 60
+    val rpm get() = encoderOuttake.getPositionAndVelocity().velocity / 28.0 * 60.0
 
     var rpmFar : Double = 3280.0
     var rpmClose : Double = 2830.0
@@ -85,7 +85,7 @@ class Shooter(
             motorTurret.power = value
         }
 
-    private var offset = 0
+    private var offset = 0.0
 
     val turretPosition get() = encoderTurret.getPositionAndVelocity().position - offset
 
@@ -137,7 +137,7 @@ class Shooter(
     )
 
     fun updateTurretPos(deltaTime: Duration, error: Double) {
-        targetPos += error * ((ShooterConfig.ticksPerRev * 108/15) / 360)
+        targetPos += error * ((ShooterConfig.ticksPerRev * (108.0/15.0)) / 360.0)
         if (targetPos > ShooterConfig.maxTurretPosition - ShooterConfig.limitTolerence) {
             targetPos = ShooterConfig.minTurretPosition
         }
